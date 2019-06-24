4e75f547611a80393dbc5612512a3dd03057a383
Jarlyyn <jarlyyn@qq.com>
2019-06-24 19:47:45 +0800

    update

--- config/config_test.go
+++ config/config_test.go
@@ -1,7 +1,17 @@
 package config
 
 import "testing"
+import "github.com/herb-go/util"
 
 func TestJSON(t *testing.T) {
-
+	type DataStruct struct {
+		Data string
+	}
+	var jdata = `//comment
+	{"Data":"12345"}`
+	var data = &DataStruct{}
+	MustLoadJSON(util.FileObjectText(jdata), data)
+	if data.Data != "12345" {
+		t.Fatal(data)
+	}
 }
