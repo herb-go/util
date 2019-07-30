d25fa541b7c9ad62ac5a6eb263f59e1153d626ca
Jarlyyn <jarlyyn@qq.com>
2019-07-31 01:18:55 +0800

    update

--- messages/formmsg/formmsg.go
+++ messages/formmsg/formmsg.go
@@ -5,6 +5,20 @@ import (
 )
 
 //MsgRequired message that shows field is required
-var MsgRequired = translate.NewMessage("form", "%[1]s required")
+var MsgRequired = translate.NewMessage("herbgo.form", "%[1]s required")
 
-var MsgNotUnique = translate.NewMessage("form", " %[1]s not unique")
+var MsgExist = translate.NewMessage("herbgo.form", " %[1]s existed")
+
+var MsgFormatError = translate.NewMessage("herbgo.form", " %[1]s format error")
+
+var MsgTooSmall = translate.NewMessage("herbgo.form", " %[1]s too small")
+
+var MsgTooLarge = translate.NewMessage("herbgo.form", " %[1]s too large")
+
+var MsgTooLong = translate.NewMessage("herbgo.form", " %[1]s too long")
+
+var MsgTooShort = translate.NewMessage("herbgo.form", " %[1]s too short")
+
+var MsgNotAvaliable = translate.NewMessage("herbgo.form", " %[1]s is not avaliable")
+
+var MsgNotMatch = translate.NewMessage("herbgo.form", "%[1]s is not match")
