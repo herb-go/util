401e679ed88fe8c90017ece374ce725b33a23e77
Jarlyyn <jarlyyn@qq.com>
2019-08-05 19:55:22 +0800

    update

--- form/commonform/msg.go
+++ form/commonform/msg.go
@@ -5,46 +5,46 @@ import (
 )
 
 //MsgRequired message that shows field is required
-var MsgRequired = ui.NewMessage("herbgo.form", "%[1]s is required.")
+var MsgRequired = ui.NewMessage("herbgo.form", "{{label}} is required.")
 
 //MsgDuplicated messages that shows field is  duplicated.
-var MsgDuplicated = ui.NewMessage("herbgo.form", " %[1]s is duplicated.")
+var MsgDuplicated = ui.NewMessage("herbgo.form", " {{label}} is duplicated.")
 
 //MsgInvalide messages that shows field is  invalid.
-var MsgInvalide = ui.NewMessage("herbgo.form", " %[1]s is invalid.")
+var MsgInvalide = ui.NewMessage("herbgo.form", "{{label}} is invalid.")
 
 //MsgNotMatch messages that shows field is  not match.
-var MsgNotMatch = ui.NewMessage("herbgo.form", "%[1]s is not match.")
+var MsgNotMatch = ui.NewMessage("herbgo.form", "{{label}} is not match.")
 
 //MsgIncorrect messages that shows field is  incorrect.
-var MsgIncorrect = ui.NewMessage("herbgo.form", "%[1]s is incorrect.")
+var MsgIncorrect = ui.NewMessage("herbgo.form", "{{label}} is incorrect.")
 
 //NewFormatWrongMsg create new message that shows field format wrong with given format.
 func NewFormatWrongMsg(format string) *ui.TemplateMessage {
-	return ui.NewTemplateMessage("herbgo.form", "%[1]s format wrong(correct format is '{{format}}'.", map[string]string{"format": format})
+	return ui.NewTemplateMessage("herbgo.form", "{{label}} format wrong(correct format is '{{format}}'.", map[string]string{"format": format})
 }
 
 //NewTooSmallMsg create new message that shows field is too small with given min value.
 func NewTooSmallMsg(min string) *ui.TemplateMessage {
-	return ui.NewTemplateMessage("herbgo.form", "%[1]s is too small (minimum is {{min}}).", map[string]string{"min": min})
+	return ui.NewTemplateMessage("herbgo.form", "{{label}} is too small (minimum is {{min}}).", map[string]string{"min": min})
 }
 
 //NewTooBigMsg create new message that shows field is too big with given max value.
 func NewTooBigMsg(max string) *ui.TemplateMessage {
-	return ui.NewTemplateMessage("herbgo.form", "%[1]s is too big (maximum is {{max}}).", map[string]string{"max": max})
+	return ui.NewTemplateMessage("herbgo.form", "{{label}} is too big (maximum is {{max}}).", map[string]string{"max": max})
 }
 
 //NewTooShortMsg create new message that shows field is too show with given min length.
 func NewTooShortMsg(min string) *ui.TemplateMessage {
-	return ui.NewTemplateMessage("herbgo.form", "%[1]s is too short (minimum is {{min}} characters).", map[string]string{"min": min})
+	return ui.NewTemplateMessage("herbgo.form", "{{label}} is too short (minimum is {{min}} characters).", map[string]string{"min": min})
 }
 
 //NewTooLongMsg create new message that shows field is too long with given max length.
 func NewTooLongMsg(max string) *ui.TemplateMessage {
-	return ui.NewTemplateMessage("herbgo.form", "%[1]s is too long (maximum is {{max}} characters).", map[string]string{"max": max})
+	return ui.NewTemplateMessage("herbgo.form", "{{label}} is too long (maximum is {{max}} characters).", map[string]string{"max": max})
 }
 
 //NewWrongLengthMsg create new message that shows field length wrong with given  length.
 func NewWrongLengthMsg(length string) *ui.TemplateMessage {
-	return ui.NewTemplateMessage("herbgo.form", " %[1]s is of the wrong length (should be {{length}} characters).", map[string]string{"length": length})
+	return ui.NewTemplateMessage("herbgo.form", " {{label}} is of the wrong length (should be {{length}} characters).", map[string]string{"length": length})
 }
--- form/commonform/validate.go
+++ form/commonform/validate.go
@@ -2,18 +2,20 @@ package commonform
 
 import (
 	"strconv"
+	"time"
+
+	"github.com/herb-go/herb/ui/validator"
 
 	"github.com/herb-go/herb/ui"
-	"github.com/herb-go/util/form"
 )
 
 //SetTranslatedFieldLabels set translated field labels to form.
-func SetTranslatedFieldLabels(form form.TranslateableForm, module string, labels map[string]string) {
-	form.SetFieldLabelsCollection(ui.GetMessages(form.Lang(), "app").Collection((labels)))
+func SetTranslatedFieldLabels(form validator.Fields, module string, labels map[string]string) {
+	form.SetComponentLabels(ui.GetMessages(form.Lang(), "app").Collection((labels)))
 }
 
 //ValidateStringLength validate string length
-func ValidateStringLength(form form.TranslateableForm, value string, field string, min int, max int) {
+func ValidateStringLength(form validator.Fields, value string, field string, min int, max int) {
 	l := len(value)
 	if min == max {
 		if l != min {
@@ -29,7 +31,7 @@ func ValidateStringLength(form form.TranslateableForm, value string, field strin
 }
 
 //ValidateIntRange validate int value range
-func ValidateIntRange(form form.TranslateableForm, value int, field string, min int, max int) {
+func ValidateIntRange(form validator.Fields, value int, field string, min int, max int) {
 	if value < min {
 		form.AddErrorf(field, NewTooSmallMsg(strconv.Itoa(min)).Translate(form.Lang()))
 
@@ -39,7 +41,7 @@ func ValidateIntRange(form form.TranslateableForm, value int, field string, min 
 }
 
 //ValidateInt64Range validate int value range
-func ValidateInt64Range(form form.TranslateableForm, value int64, field string, min int64, max int64) {
+func ValidateInt64Range(form validator.Fields, value int64, field string, min int64, max int64) {
 	if value < min {
 		form.AddErrorf(field, NewTooSmallMsg(strconv.FormatInt(min, 10)).Translate(form.Lang()))
 
@@ -49,29 +51,50 @@ func ValidateInt64Range(form form.TranslateableForm, value int64, field string, 
 }
 
 //ValidateRequiredPointer validate required pointer field
-func ValidateRequiredPointer(form form.TranslateableForm, value interface{}, field string) {
+func ValidateRequiredPointer(form validator.Fields, value interface{}, field string) {
 	if value == nil {
 		form.AddErrorf(field, MsgRequired.Translate(form.Lang()))
 	}
 }
 
 //ValidateRequiredString validate required string field
-func ValidateRequiredString(form form.TranslateableForm, value string, field string) {
+func ValidateRequiredString(form validator.Fields, value string, field string) {
 	if value == "" {
 		form.AddErrorf(field, MsgRequired.Translate(form.Lang()))
 	}
 }
 
 //ValidateRequiredInt validate required int field
-func ValidateRequiredInt(form form.TranslateableForm, value int, field string) {
+func ValidateRequiredInt(form validator.Fields, value int, field string) {
 	if value == 0 {
 		form.AddErrorf(field, MsgRequired.Translate(form.Lang()))
 	}
 }
 
 //ValidateRequiredInt64 validate required int64 field
-func ValidateRequiredInt64(form form.TranslateableForm, value int64, field string) {
+func ValidateRequiredInt64(form validator.Fields, value int64, field string) {
 	if value == 0 {
+		form.AddErrorf(field, MsgRequired.Translate(form.Lang()))
+	}
+}
+
+//ValidateRequiredFloat32 validate required int field
+func ValidateRequiredFloat32(form validator.Fields, value float32, field string) {
+	if value == 0 {
+		form.AddErrorf(field, MsgRequired.Translate(form.Lang()))
+	}
+}
+
+//ValidateRequiredFloat64 validate required int64 field
+func ValidateRequiredFloat64(form validator.Fields, value float64, field string) {
+	if value == 0 {
+		form.AddErrorf(field, MsgRequired.Translate(form.Lang()))
+	}
+}
+
+//ValidateRequiredTime validate required int64 field
+func ValidateRequiredTime(form validator.Fields, value time.Time, field string) {
+	if value.IsZero() {
 		form.AddErrorf(field, MsgRequired.Translate(form.Lang()))
 	}
 }
