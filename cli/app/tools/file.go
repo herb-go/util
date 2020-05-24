6ca4423bf5f6be75d626a33f2fc8c7bdad46ddf5
jarlyyn <jarlyyn@qq.com>
2020-05-24 15:41:19 +0800

    update

--- cli/app/tools/file.go
+++ cli/app/tools/file.go
@@ -1,8 +1,11 @@
 package tools
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
+
+	"github.com/herb-go/util"
 )
 
 func FileExists(pathlist ...string) (bool, error) {
@@ -27,4 +30,19 @@ func IsFolder(pathlist ...string) (bool, error) {
 		return false, err
 	}
 	return mode.IsDir(), nil
+}
+
+func CopyIfNotExist(src string, target ...string) error {
+	result, err := FileExists(target...)
+	if err != nil {
+		return err
+	}
+	if result {
+		return nil
+	}
+	bs, err := ioutil.ReadFile(src)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filepath.Join(target...), bs, util.DefaultFileMode)
 }
